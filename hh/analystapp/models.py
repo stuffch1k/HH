c09d329eefae13bb3f894a63dc7fbc9794a998f1
stuffch1k <elizaveta.sineva@urfu.me>
2023-03-02 14:05:27 +0500

    HH-3

--- hh/analystapp/models.py
+++ hh/analystapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    description = models.TextField(blank=True, null=True)
+    skill = models.ForeignKey('Skill',on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    
---
+++ hh/analystapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls import handler404
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+from .views import *
+
+urlpatterns = [
+    path('',index,name='home'),
+]
