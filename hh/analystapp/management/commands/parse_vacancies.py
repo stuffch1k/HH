a8793e6c2d4b357ed22b2cdd28db97c704bba985
WheatleyHDD <wheatleyhdd@gmail.com>
2023-03-03 13:45:19 +0500

    Попытка исправить парсер

--- hh/analystapp/management/commands/parse_vacancies.py
+++ hh/analystapp/management/commands/parse_vacancies.py
@@ -21,28 +21,35 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Начало парсинга вакансий")
 
+        # разделяем по временным интервалам
         date_from = datetime.datetime.now() - datetime.timedelta(days=options['days'])
         time = pd.date_range(date_from, datetime.datetime.now(), freq="4H")
 
+        # ебучие пи... потоки
+        threads = []
         for i, t in enumerate(time):
             if i + 1 < len(time):
-                thread = threading.Thread(target=self.get_task, args=([t, time[i + 1]], ))
-                thread.start()
-                thread.join()
+                t = threading.Thread(target=self.get_task, args=([t, time[i + 1]], i))
+                threads.append(t)
+                t.start()
+
+        [thread.join() for thread in threads]
 
         self.stdout.write(self.style.SUCCESS("Конец работы"))
 
-    def get_task(self, freq):
+    def get_task(self, freq, thread_num):
+        self.stdout.write(self.style.SUCCESS(f"Начало работы потока {thread_num}"))
         result = []
+        # если страниц несколько
         for p in range(0, 20 + 1):
             out = self.get_post(freq[0], freq[1], p)
             if len(out) == 0: break
             result += out
-        self.on_end_parse(result)
+        self.on_end_parse(result, thread_num)
 
     def get_post_desc_and_skills(self, post_id):
         full_info = requests.get(API_URL.format('vacancies') + '/' + post_id, headers=HEADER).json()
-        return full_info['description'], full_info['key_skills']
+        return full_info['description'] if 'description' in full_info else "", full_info['key_skills'] if 'key_skills' in full_info else []
 
     def get_post(self, date_from, date_to, page=0):
         result = []
@@ -55,11 +62,14 @@ class Command(BaseCommand):
         }
         resp = requests.get(API_URL.format('vacancies'), headers=HEADER, params=params)
         for i in resp.json()['items']:
+            # Отдельным запросом нужно получить блядские навыки
             others = self.get_post_desc_and_skills(i['id'])
             result.append({'name': i['name'], 'description': others[0], 'key_skills': others[1]})
         return result
 
-    def on_end_parse(self, response):
+    def on_end_parse(self, response, tnum):
+        self.stdout.write(self.style.SUCCESS(f"Завершение работы потока {tnum}"))
+        # заполняем данные в бд
         for r in response:
             vac = Vacancy.objects.create(name=r['name'], description=r['description'])
             for ks in r['key_skills']: vac.skill.create(name=ks['name'])
